5e72d33bd8d26946fbf4958541b4dce3e393ac4e
Danial Motevali <danial.motevali.82@gmail.com>
2023-07-11 18:49:54 +0330

    Add project files.

---
+++ Controllers/HelloController1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HelloController1 : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            
+            Session["Username"] = model.Username;
+
+            HttpCookie cookie = new HttpCookie("Username", model.Username);
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
